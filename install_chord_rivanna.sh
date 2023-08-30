b120e0638d34d3bbad5f9c2444f95ea703eb25c8
Andrew Davis <andrew.davis1104@gmail.com>
2023-08-30 10:49:48 -0400

    added some new features to rivanna install

--- install_chord_rivanna.sh
+++ install_chord_rivanna.sh
@@ -35,20 +35,45 @@ chombo_lib_path=$chord_chombo_path/Chombo/lib/mk
 hdf5_path=$install_path/hdf5-1.10.6/1.10.6
 cgns_path=$install_path/CGNS/4.4.0
 module load gcc/9.2.0  openmpi/3.1.6
+#used for warnings
+hdf5source=1
+hdf5install=1
+cgnssource=1
+cgnsinstall=1
 #step 1 setup hdf5
-cd $install_path
-git clone $hdf5_git
-cd $install_path/hdf5-1.10.6
-CC=mpicc ./configure --enable-fortran --enable-parallel --enable-shared --prefix=$hdf5_path
-make
-make install
+#if you dont have the hdf5 source code, grab it
+if [! -d "$install_path/hdf5-1.10.6"];
+then
+    hdf5source=0 #used for warnings
+    cd $install_path
+    git clone $hdf5_git
+fi
+#if you dont have the hdf5 lib installed, install it
+if [! -d "$hdf5_path/lib"];
+then
+    hdf5install=0 #used for warnings
+    cd $install_path/hdf5-1.10.6
+    CC=mpicc ./configure --enable-fortran --enable-parallel --enable-shared --prefix=$hdf5_path
+    make
+    make install
+fi
 #step 2 setup cgns
-cd $install_path
-git clone -b master $cgns_git
-cd $install_path/CGNS/src
-CC=mpicc LIBS="-ldl -lz" ./configure --prefix=$cgns_path --with-hdf5=$hdf5_path  --enable-parallel
-make
-make install
+#if you dont have the cgns source code, grab it
+if [! -d "$install_path/CGNS"];
+then
+    cgnssource=0 #used for warnings
+    cd $install_path
+    git clone -b master $cgns_git
+fi
+#if you dont have the cgns lib installed, install it
+if [! -d "$cgns_path/lib"];
+then
+    cgnsinstall=0 #used for warnings
+    cd $install_path/CGNS/src
+    CC=mpicc LIBS="-ldl -lz" ./configure --prefix=$cgns_path --with-hdf5=$hdf5_path  --enable-parallel
+    make
+    make install
+fi
 #step 3 grab chord and Chombo
 cd $chord_chombo_path
 git clone $chombo_git
@@ -65,3 +90,16 @@ sed -i "s,HDFMPIINCFLAGS   =,HDFMPIINCFLAGS   = -I$hdf5_path/include,1" Make.def
 sed -i "s,HDFMPILIBFLAGS   = -lhdf5 -lsz -lz -ldl,HDFMPILIBFLAGS   = -L$hdf5_path/lib -lhdf5 -lz,1" Make.defs.local
 sed -i "s,CGNSINCFLAGS =,CGNSINCFLAGS = -I$cgns_path/include,1" Make.defs.local
 sed -i "s,CGNSLIBFLAGS = -lcgns, CGNSLIBFLAGS = -L$cgns_path/lib -lcgns,1" Make.defs.local
+
+if [[$hdf5source -eq 1]]; then
+echo "WARNING: it appears you already have the hdf5-1.10.6 soure code installed... skipping grabbing it"
+fi
+if [[$hdf5install -eq 1]]; then
+echo "WARNING: it appears you already have the hdf5-1.10.6 library installed... skipping installation"
+fi
+if [[$cgnssource -eq 1]]; then
+echo "WARNING: it appears you already have the cgns-4.4.0 soure code installed... skipping grabbing it"
+fi
+if [[$cgnsinstall -eq 1]]; then
+echo "WARNING: it appears you already have the cgns-4.4.0 library installed... skipping installation"
+fi
